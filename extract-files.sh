4888ec9c0230a66b3164dfd7f18f4cf575917187
cynagenautes <38454938+cynagenautes@users.noreply.github.com>
2024-10-27 04:34:50 +0900

    Revert "diting: Rename audio.primary.taro to audio.primary.taro-diting to avoid conflicts"
    This reverts commit 41106b3d7a43a2f12889965a963349e5a6113d61.

--- extract-files.sh
+++ extract-files.sh
@@ -20,10 +20,6 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --set-soname "libalNN_snpe.so" "${2}"
             ;;
-        vendor/lib64/hw/audio.primary.taro-diting.so)
-            [ "$2" = "" ] && return 0
-            "${PATCHELF_0_17_2}" --set-soname "audio.primary.taro-diting.so" "${2}"
-            ;;
         vendor/lib64/hw/fingerprint.goodix_fod.default.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --set-soname "fingerprint.goodix_fod.default.so" "${2}"
@@ -31,10 +27,6 @@ function blob_fixup() {
         vendor/lib64/libcamximageformatutils.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --replace-needed "vendor.qti.hardware.display.config-V2-ndk_platform.so" "vendor.qti.hardware.display.config-V2-ndk.so" "${2}"
-            ;;
-        vendor/lib64/libkaraokepal.so)
-            [ "$2" = "" ] && return 0
-            "${PATCHELF_0_17_2}" --replace-needed "audio.primary.taro.so" "audio.primary.taro-diting.so" "${2}"
             ;;
         *)
             return 1
